cf6b451d7a2d877bf94022575e6b92b6a83db011
dmanchola <dmanchola@alphaig.com>
2026-02-25 17:01:21 -0500

    git limpieza de librerias

--- src/conexion.class.php
+++ src/conexion.class.php
@@ -1,6 +1,5 @@
 <?php
 include_once('setex-config.php');
-include_once(LIBSPATH.'adodb/adodb.inc.php');
 
     function conexion() {
         $dbhost = "alpha-msj-db-server-dev.celntjvopzqm.us-west-2.rds.amazonaws.com";
